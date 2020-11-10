dc18aa358d82c6fddda8027de5c41e90e9887b0c
davien21 <chidiebereekennia@gmail.com>
2020-11-10 23:25:24 +0100

    single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -28,7 +28,6 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props)
     return (
       <div className="">
         <span style={this.styles} className={this.getBadgeClasses()}>
@@ -41,7 +40,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete

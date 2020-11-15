a8a765f1dee242bb7124913106e0c28bc715c8f5
davien21 <chidiebereekennia@gmail.com>
2020-11-15 20:45:44 +0100

    move state and methods up to parent

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,27 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    tags: ["tag1", "tag2", "tag3"],
-  };
-
-  handleIncrement = (product) => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   styles = {
     fontSize: "13px",
     fontWeight: "bold",
@@ -34,7 +13,7 @@ class Counter extends Component {
           {this.formatvalue()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -45,20 +24,18 @@ class Counter extends Component {
         >
           Delete
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {/* {this.renderTags()} */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatvalue() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }

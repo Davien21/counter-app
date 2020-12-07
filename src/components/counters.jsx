02267a97fdf18bad7f3319b7cd94ea243a2236b1
davien21 <chidiebereekennia@gmail.com>
2020-12-07 03:07:26 +0100

    add decrement feature

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,7 +3,7 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
-    const { onReset, onDelete, onIncrement, counters } = this.props
+    const { onReset, onDelete, onIncrement, onDecrement, counters } = this.props
     
     return (
       <div>
@@ -15,6 +15,7 @@ class Counters extends Component {
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           />
         ))}
